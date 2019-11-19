f4a5b46f3834b614e240c137be4e244fc22bb019
betitomiller <betitomiller@gmail.com>
2019-11-19 21:44:29 +0200

    funtion lab

--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,11 +1,7 @@
 'use strict';
 
 const ipToInt = (ip = '127.0.0.1') => {
-  // Parse ip address as string, for example '10.0.0.1'
-  // to ['10', '0', '0', '1'] to [10, 0, 0, 1]
-  // and convert to Number value 167772161 with sitwise shift
-  // (10 << 8 << 8 << 8) + (0 << 8 << 8) + (0 << 8) + 1 === 167772161
-  // Use Array.prototype.reduce of for loop
+  const reducer = (accum, curVal) => (accum << 8) + +curVal;
+  return (ip.split('.').reduce(reducer));
 };
-
 module.exports = { ipToInt };
